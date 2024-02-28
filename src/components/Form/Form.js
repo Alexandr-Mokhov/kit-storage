43b88f1c89537d8c35a2369cb2d2f007fda9b67b
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-02-28 12:56:42 +0500

    feat: создать логику сбора данных из инпутов, добавить поле имени для регистрации

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,37 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './Form.css';
 
-function onSubmit() { }
-
 export default function Form({ name, title, buttonText }) {
+	const [values, setValues] = useState({});
+
+	const handleChange = (evt) => {
+		const target = evt.target;
+		const name = target.name;
+		const value = target.value;
+		setValues({ ...values, [name]: value });
+	}
+
+	function handleSubmit(evt) {
+		evt.preventDefault();
+		console.log(values);
+	}
+
 	return (
-		<form className="form" onSubmit={onSubmit} name={name} noValidate>
+		<form className="form" onSubmit={handleSubmit} name={name}>
 			<h2 className="form__title">{title}</h2>
+			{name === "register" &&
+				<input
+					id="input-name"
+					className="form__input"
+					name="name"
+					type="text"
+					placeholder="Ваше имя"
+					required
+					value={values['name'] || ''}
+					onChange={handleChange}
+					autoComplete="off"
+				/>}
 			<input
 				id="input-email"
 				className="form__input"
@@ -14,8 +39,8 @@ export default function Form({ name, title, buttonText }) {
 				type="email"
 				placeholder="E-mail"
 				required
-				// value={values['email'] || ''}
-				// onChange={handleChange}
+				value={values['email'] || ''}
+				onChange={handleChange}
 				autoComplete="off"
 			/>
 			<input
@@ -26,16 +51,16 @@ export default function Form({ name, title, buttonText }) {
 				placeholder="Пароль"
 				required
 				minLength="4"
-				// value={values['password'] || ''}
-				// onChange={handleChange}
+				value={values['password'] || ''}
+				onChange={handleChange}
 				autoComplete="off"
 			/>
 			<button className="form__button" type="submit">
 				{buttonText}
 			</button>
-			{name === "register" ? 
-			<Link className="form__link" to="/login">Уже зарегистрированы? Войти</Link> :
-			<Link className="form__link" to="/register">Зарегистрироваться</Link>}
+			{name === "register" ?
+				<Link className="form__link" to="/login">Уже зарегистрированы? Войти</Link> :
+				<Link className="form__link" to="/register">Зарегистрироваться</Link>}
 		</form>
 	);
 }
