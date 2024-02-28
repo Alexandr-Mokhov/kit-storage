3e96a0bf06a1c5c93c1a511f8481726155c9cea3
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-02-28 10:49:53 +0500

    feat: почистить структуру, настроить header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+		<header className="header">
+			<div className="header__logo">
+				Kit Storage
+			</div>
+		</header>
+  );
+}
+
+export default Header;
