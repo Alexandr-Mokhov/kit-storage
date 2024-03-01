ccb53c8d95252a1ef35528a284e7f954ce374c1c
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-03-01 14:15:43 +0500

    feat: добавить логику удаления файлов

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,22 +1,26 @@
-import './File.css';
 import iconFile from '../../images/icon-file.png';
+import './File.css';
 
-export default function File({ name, url, mimeType }) {
+export default function File({ file, handleDeleteClick }) {
 	function setSrc() {
-		if(mimeType.includes('image')) {
-			return url;
+		if (file.mimeType.includes('image')) {
+			return file.url;
 		} else {
 			return iconFile;
 		}
 	}
-	
+
+	function onFileDelete() {
+		handleDeleteClick(file);
+	}
+
 	return (
 		<div className="file">
-			<p className="file__name">{name}</p>
+			<p className="file__name">{file.name}</p>
 			<button className="file__button file__button_download">
-				<img className="file__img" src={setSrc()} alt={name} />
+				<img className="file__img" src={setSrc()} alt={file.name} />
 			</button>
-			<button className="file__button file__button_delete">
+			<button className="file__button file__button_delete" onClick={onFileDelete}>
 				<div className="file__icon-delete" />
 			</button>
 		</div>
--- src/components/FileContainer/FileContainer.js
+++ src/components/FileContainer/FileContainer.js
@@ -1,16 +1,28 @@
-import './FileContainer.css';
-import arrFiles from '../../arrFiles';
+import { useEffect, useState } from 'react';
+import initialFiles from '../../initialFiles';
 import File from '../File/File';
+import './FileContainer.css';
+
+
 
 export default function FileContainer() {
+	const [arrFiles, setArrFiles] = useState([]);
+
+	useEffect(() => {
+		setArrFiles(initialFiles);
+	}, []);
+
+	function handleDeleteClick(file) {
+		setArrFiles((state) => state.filter(item => item.id !== file.id));
+	}
+
 	return (
 		<section className="file-container">
 			{arrFiles.map((file) => {
 				return <File
-					name={file.name}
-					url={file.url} 
-					mimeType={file.mimeType}
+					file={file}
 					key={file.id}
+					handleDeleteClick={handleDeleteClick}
 				/>
 			})}
 		</section>
