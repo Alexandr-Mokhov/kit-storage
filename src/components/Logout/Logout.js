09ff4c61374f7f238a4705bde67721177a95aa52
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-03-12 13:35:48 +0500

    hotfix: удалять данные файлов из стейта при выходе

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setLoggedIn } from '../../store/slices/loggedInSlice';
+import { setAllFiles } from '../../store/slices/allFilesSlice';
 import './Logout.css';
 
 export default function Logout() {
@@ -9,6 +10,7 @@ export default function Logout() {
 	function onSignOut() {
 		dispatch(setLoggedIn(false));
 		localStorage.removeItem('token');
+		dispatch(setAllFiles([]));
 	}
 
 	return (
